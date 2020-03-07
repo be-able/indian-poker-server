29fae98646e41a42ffeafa330b993349fbfbfb82
Horace Velmont <dragond7@naver.com>
2020-03-07 16:20:02 +0900

    add initial files

---
+++ game/src/main/java/com/beable/poker/game/message/Greeting.java
@@ -0,0 +1,18 @@
+package com.beable.poker.game.message;
+
+public class Greeting {
+
+	private String content;
+
+	public Greeting() {
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
---
+++ game/src/main/java/com/beable/poker/game/message/game/Game.java
@@ -0,0 +1,4 @@
+package com.beable.poker.game.message.game;
+
+public class Game {
+}
