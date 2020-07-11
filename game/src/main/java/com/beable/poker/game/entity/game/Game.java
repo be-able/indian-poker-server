c01958deb8cdf0dfac3736908a25f06b3bf6aa01
Horace Velmont <dragond7@naver.com>
2020-07-11 21:59:03 +0900

    임시 테스트를 위해 실패 테스트 주석

--- game/src/main/java/com/beable/poker/game/entity/game/Game.java
+++ game/src/main/java/com/beable/poker/game/entity/game/Game.java
@@ -2,18 +2,21 @@ package com.beable.poker.game.entity.game;
 
 import com.beable.poker.game.entity.BaseEntity;
 import com.beable.poker.game.entity.round.Round;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @SuperBuilder
 @NoArgsConstructor
 public class Game extends BaseEntity {
     @OneToMany
-    private List<Round> roundList = new ArrayList<>();
+    private List<Round> roundList;
 }
--- game/src/test/java/com/beable/poker/game/GameApplicationTests.java
+++ game/src/test/java/com/beable/poker/game/GameApplicationTests.java
@@ -2,12 +2,14 @@ package com.beable.poker.game;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GameApplicationTests {
-	
-	@Test
-	void contextLoads() {
-	}
-	
+
+    @Test
+    void contextLoads() {
+    }
+
 }
